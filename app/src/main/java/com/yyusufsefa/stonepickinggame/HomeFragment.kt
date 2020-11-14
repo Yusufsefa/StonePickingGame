1c8551560af9c490a9f060b26a6d5820d394d9cf
Yusuf Sefa ALİŞ <yusufsefa61@gmail.com>
2020-11-14 16:31:28 +0300

    Design Home and PrepareLevel

---
+++ app/src/main/java/com/yyusufsefa/stonepickinggame/HomeFragment.kt
@@ -0,0 +1,5 @@
+package com.yyusufsefa.stonepickinggame
+
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment(R.layout.fragment_home)
---
+++ app/src/main/java/com/yyusufsefa/stonepickinggame/PrepareLevel.kt
@@ -0,0 +1,5 @@
+package com.yyusufsefa.stonepickinggame
+
+import androidx.fragment.app.Fragment
+
+class PrepareLevel : Fragment(R.layout.fragment_prepare_level)
