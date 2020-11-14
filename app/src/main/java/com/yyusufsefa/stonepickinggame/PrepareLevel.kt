49679dc1f67d91db1bbe36f9969fa0321fb73b84
Ramazan Öğünç <ramazanogunc@hotmail.com>
2020-11-14 20:09:55 +0300

    finish toggle button single selected

--- app/src/main/java/com/yyusufsefa/stonepickinggame/PrepareLevel.kt
+++ app/src/main/java/com/yyusufsefa/stonepickinggame/PrepareLevel.kt
@@ -1,16 +1,61 @@
 package com.yyusufsefa.stonepickinggame
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_prepare_level.*
 
-class PrepareLevel : Fragment(R.layout.fragment_prepare_level){
+class PrepareLevel : Fragment(R.layout.fragment_prepare_level) {
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        gridView.adapter = StoneAdapter(requireContext(), MockList.getMockList())
+        gridView.adapter = StoneAdapter(
+            requireContext(),
+            MockList.getMockList(),
+            ::onGridViewItemClick
+        )
+
+        initToggles()
     }
 
+
+    private fun onGridViewItemClick(gridItemList: List<GridItem>, clickedItem: GridItem) {
+        Toast.makeText(
+            requireContext(),
+            clickedItem.x.toString() + " " + clickedItem.y,
+            Toast.LENGTH_LONG
+        ).show()
+    }
+
+
+    private fun initToggles() {
+        toggleMainStone.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                toggleNormalStone.isChecked = false
+                toggleWallStone.isChecked = false
+            }
+        }
+        toggleNormalStone.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                toggleMainStone.isChecked = false
+                toggleWallStone.isChecked = false
+            }
+        }
+        toggleWallStone.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                toggleMainStone.isChecked = false
+                toggleNormalStone.isChecked = false
+            }
+        }
+    }
+
+
 }
+
+
+
--- app/src/main/java/com/yyusufsefa/stonepickinggame/StoneAdapter.kt
+++ app/src/main/java/com/yyusufsefa/stonepickinggame/StoneAdapter.kt
@@ -7,21 +7,30 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 
-class StoneAdapter(val context: Context, val gridItem: List<GridItem>) : BaseAdapter() {
+class StoneAdapter(
+    private val context: Context,
+    private val gridItemList: List<GridItem>,
+    private val onClick: (gridItemList: List<GridItem>, clickedItem: GridItem) -> Unit
+) : BaseAdapter() {
 
-    override fun getCount(): Int = gridItem.size
+    override fun getCount(): Int = gridItemList.size
 
-    override fun getItem(p0: Int): Any = gridItem[p0]
+    override fun getItem(p0: Int): Any = gridItemList[p0]
 
     override fun getItemId(p0: Int): Long = 0
 
     @SuppressLint("ViewHolder")
     override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
-        val temp = if (gridItem[p0].isBlack)
+        val item = gridItemList[p0]
+        val temp = if (item.isBlack)
             R.layout.blackboard
         else
             R.layout.whiteboard
-        return LayoutInflater.from(context).inflate(temp, p2, false)
+        val view = LayoutInflater.from(context).inflate(temp, p2, false)
+        view.setOnClickListener{
+            onClick(gridItemList,item)
+        }
+        return view
     }
 
 }
